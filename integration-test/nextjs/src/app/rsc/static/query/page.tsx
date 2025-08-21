b9a22bd03fdf6daf3896420797a4882bdaa9450a
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-08-21 14:50:56 +0200

    fix nextjs build

--- integration-test/nextjs/src/app/rsc/static/query/page.tsx
+++ integration-test/nextjs/src/app/rsc/static/query/page.tsx
@@ -21,10 +21,6 @@ export const dynamic = "force-static";
 export default async function Home() {
   const { data } = await query({ query: QUERY });
   return (
-    <ul>
-      {data.products.map(({ id, title }) => (
-        <li key={id}>{title}</li>
-      ))}
-    </ul>
+    <ul>{data?.products.map(({ id, title }) => <li key={id}>{title}</li>)}</ul>
   );
 }
