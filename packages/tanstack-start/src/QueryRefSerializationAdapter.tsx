f819ca405a07653acd7812e97a5cd93dbfccf8c9
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-09-02 09:53:24 +0200

    workaround instead of patch

--- packages/tanstack-start/src/QueryRefSerializationAdapter.tsx
+++ packages/tanstack-start/src/QueryRefSerializationAdapter.tsx
@@ -14,7 +14,19 @@ export const getQueryRefSerializationAdapter = (apolloClient: ApolloClient) =>
     key: "apollo-query-ref",
     test: isTransportedQueryRef,
     fromSerializable(value) {
-      const queryRef = { $__apollo_queryRef: value };
+      const queryRef = {
+        $__apollo_queryRef: {
+          ...value,
+          stream: value.stream.pipeThrough(new TransformStream(), {
+            /**
+             * This is a workaround for an issue with seroval streams that will keep writing
+             * incoming chunks/close calls, even if the consumer called `reader.cancel()`.
+             * Might get fixed upstream with https://github.com/lxsmnsyc/seroval/pull/62
+             */
+            preventCancel: true,
+          }),
+        },
+      };
       reviveTransportedQueryRef(queryRef, apolloClient);
       return queryRef;
     },
